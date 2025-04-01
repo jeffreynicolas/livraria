bcd37d9d41dccbef826dd04077b3484194ff94e3
jeffreynicolas <jeffreynicolas.deus@gmail.com>
2025-04-01 13:52:32 -0300

    quase

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    descrição = models.CharField(max_length=100)
-
+    descricao = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.descricao
